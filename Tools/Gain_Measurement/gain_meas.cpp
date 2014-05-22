737ae50e0d3fab2cc4b56b61a89959e00f441d7b
weinstock <weinstock@physik.rwth-aachen.de>
2014-05-22 13:56:28 +0200

    Added gain measurement tool.

---
+++ Tools/Gain_Measurement/gain_meas.cpp
@@ -0,0 +1,124 @@
+// clib + unix
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <iostream>
+#include <math.h>
+// liblab
+#include <rs232/linux_rs232.h>
+#include <rs232/TEKTRONIX/TDS3000.h>
+// root
+#include <TH1F.h>
+#include <TF1.h>
+#include <TCanvas.h>
+#include <TTree.h>
+#include <TFile.h>
+
+using namespace std;
+
+// determines the baseline by fitting a line to the first n points of the histo
+// slope and offset determine the quality of the fit
+// (slope ~= 0, offset ~= 0)
+float getBaseline(TH1F* histo, float start, float stop, float &slope, float &offset);
+
+int main(int argc, char** argv){
+	int nSamples = 0;
+	float t_min = -70e-9, t_max = 130e-9;	// min and max time on the scope screen
+
+	if (argc < 3){
+		printf("Usage: ./gain_meas <nSamples> <filename> [<tmin in s> <tmax in s>]\n");
+		printf("\te.g.: ./gain_meas 1000 test.root -70e-9 130e-9\n");
+		return -1;
+	} else if (argc == 3){
+		nSamples = atoi(argv[1]);
+	} else {
+		printf("Usage: ./gain_meas <nSamples> <filename> [<tmin in s> <tmax in s>]\n");
+		printf("\te.g.: ./gain_meas 1000 test.root -70e-9 130e-9\n");
+		return -1;
+	} 
+
+	// serial port setup
+	linux_rs232 com("/dev/ttyUSB0", 38400);
+	TDS3000 scope(&com);
+
+	printf("[Gain Measurement] - Init scope..\n");
+ 
+	// scope setup
+	const int sample_len = 500;	// or 10000 (slower...)
+	scope.selectSource(TDS3000_CH1);
+	scope.setRecordLength(sample_len);
+	scope.setStartPoint(1);
+	scope.setStopPoint(sample_len);
+	scope.setWaveformDataWidth(TDS3000_8BIT);
+	// measurement is set up, get info 
+	scope.getWaveformPreamble();
+	
+	printf("[Gain Measurement] - Setting up output files..\n");
+
+	// to be saved by root
+	Float_t t[sample_len], u[sample_len];
+	Int_t len;
+	Float_t baseline, pulse_height, slope, offset;
+	// root stuff
+	TFile* out_file = new TFile(argv[2], "RECREATE");
+	TH1F* h_pulse = new TH1F("h_pulse", "h_pulse", sample_len+1, t_min, t_max);
+	TCanvas* c1 = new TCanvas();
+	TTree* out_tree = new TTree("outtree", "gain_tree");
+	out_tree->Branch("time", t, "t[500]/F");
+	out_tree->Branch("amplitude", u, "u[500]/F");
+	out_tree->Branch("len", &len);
+	out_tree->Branch("slope", &slope);
+	out_tree->Branch("offset", &offset);
+	out_tree->Branch("baseline", &baseline);
+	out_tree->Branch("pulse_height", &pulse_height);
+	
+	printf("[Gain Measurement] - Starting measurement of %i samples.\n", nSamples);
+	int iSample = 0, rejects = 0;
+	while (iSample < nSamples){
+		h_pulse->Reset();
+		len = scope.getWaveformReading(t, u);
+		usleep(1e5);
+		// fill histo
+		for (int i = 0; i < len; i++)
+			h_pulse->Fill(t[i], -1*u[i]);	
+		cout << "[Gain Measurement] - Progress " << iSample << "/" << nSamples << "\t\r" << flush;
+		// calculate baseline
+		baseline = getBaseline(h_pulse, t_min, 0, slope, offset);
+		// reject if baseline too steep
+		if ( fabs(slope) > 1e5 ){ 
+			rejects++;
+			printf("[Gain Measurement] - rejected sample, baseline too steep (%.3e).\n", fabs(slope));
+			continue;
+		}
+		// everything seems to be fine
+		pulse_height = h_pulse->GetBinContent(h_pulse->GetMaximumBin()) - baseline;
+		iSample++;
+
+		// save
+		out_tree->Fill();
+#ifdef VERBOSE
+		h_pulse->Write();
+#endif
+	}
+	printf("[Gain Measurement] - Done -  rejected %i samples (eff. %2.1f%).\n", rejects, 100.*(float)iSample/(float)(rejects + iSample) );
+
+	// cleanup
+	out_tree->Write();
+	out_file->Close();
+}
+
+float getBaseline(TH1F* histo, float start, float stop, float &slope, float&offset){
+	float baseline = 0;
+	TF1* f_baseline = new TF1("fun baseline", "[0] + [1]*x", start, stop);
+	histo->Fit(f_baseline, "Q", "", start, stop);
+	offset = f_baseline->GetParameter(0);
+	slope = f_baseline->GetParameter(1);
+	int i = 0;
+	while ( histo->GetBinContent(i) < stop ){
+		baseline += histo->GetBinContent(i);
+		i++;
+	}
+	baseline /= (float)i;
+	delete f_baseline;
+	return baseline;
+}
