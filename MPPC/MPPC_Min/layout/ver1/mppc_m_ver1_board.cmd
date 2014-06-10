89e00dd2adf1b577efc955e530b098d3a611885c
run1c <weinstock@physik.rwth-aachen.de>
2014-06-10 11:12:26 +0200

    Added new version of the pcb layout

---
+++ MPPC/MPPC_Min/layout/ver1/mppc_m_ver1_board.cmd
@@ -0,0 +1,199 @@
+# Pin name action command file
+
+# Start of element L0
+ChangePinName(L0, 1, 1)
+ChangePinName(L0, 2, 2)
+
+# Start of element CONN_FRONT2
+ChangePinName(CONN_FRONT2, 6, 6)
+ChangePinName(CONN_FRONT2, 5, 5)
+ChangePinName(CONN_FRONT2, 4, 4)
+ChangePinName(CONN_FRONT2, 3, 3)
+ChangePinName(CONN_FRONT2, 2, 2)
+ChangePinName(CONN_FRONT2, 1, 1)
+
+# Start of element TP_LP
+ChangePinName(TP_LP, 1, 1)
+
+# Start of element TP_VBIN
+ChangePinName(TP_VBIN, 1, 1)
+
+# Start of element C12
+ChangePinName(C12, 2, 2)
+ChangePinName(C12, 1, 1)
+
+# Start of element C8
+ChangePinName(C8, 2, 2)
+ChangePinName(C8, 1, 1)
+
+# Start of element R3
+ChangePinName(R3, 1, 1)
+ChangePinName(R3, 2, 2)
+
+# Start of element C4
+ChangePinName(C4, 2, 2)
+ChangePinName(C4, 1, 1)
+
+# Start of element LEMO_OUT
+ChangePinName(LEMO_OUT, 2, 2)
+ChangePinName(LEMO_OUT, 1, 1)
+
+# Start of element R12
+ChangePinName(R12, 1, 1)
+ChangePinName(R12, 2, 2)
+
+# Start of element C13
+ChangePinName(C13, 2, 2)
+ChangePinName(C13, 1, 1)
+
+# Start of element R10
+ChangePinName(R10, 1, 1)
+ChangePinName(R10, 2, 2)
+
+# Start of element R9
+ChangePinName(R9, 1, 1)
+ChangePinName(R9, 2, 2)
+
+# Start of element R11
+ChangePinName(R11, 1, 1)
+ChangePinName(R11, 2, 2)
+
+# Start of element C10
+ChangePinName(C10, 2, 2)
+ChangePinName(C10, 1, 1)
+
+# Start of element R6
+ChangePinName(R6, 1, 1)
+ChangePinName(R6, 2, 2)
+
+# Start of element R7
+ChangePinName(R7, 1, 1)
+ChangePinName(R7, 2, 2)
+
+# Start of element C2
+ChangePinName(C2, 2, 2)
+ChangePinName(C2, 1, 1)
+
+# Start of element R4
+ChangePinName(R4, 1, 1)
+ChangePinName(R4, 2, 2)
+
+# Start of element SiPM
+ChangePinName(SiPM, 2, 2)
+ChangePinName(SiPM, 1, 1)
+
+# Start of element U4
+ChangePinName(U4, 6, /SHDN)
+ChangePinName(U4, 4, V-)
+ChangePinName(U4, 10, V+)
+ChangePinName(U4, 9, OUT)
+ChangePinName(U4, 8, IN-)
+ChangePinName(U4, 7, IN+)
+ChangePinName(U4, 5, /SHDN)
+ChangePinName(U4, 4, V-)
+ChangePinName(U4, 10, V+)
+ChangePinName(U4, 1, OUT)
+ChangePinName(U4, 2, IN-)
+ChangePinName(U4, 3, IN+)
+
+# Start of element CONN_FRONT
+ChangePinName(CONN_FRONT, 6, 6)
+ChangePinName(CONN_FRONT, 5, 5)
+ChangePinName(CONN_FRONT, 4, 4)
+ChangePinName(CONN_FRONT, 3, 3)
+ChangePinName(CONN_FRONT, 2, 2)
+ChangePinName(CONN_FRONT, 1, 1)
+
+# Start of element TP_RX
+ChangePinName(TP_RX, 1, 1)
+
+# Start of element TP_TX
+ChangePinName(TP_TX, 1, 1)
+
+# Start of element TP_T
+ChangePinName(TP_T, 1, 1)
+
+# Start of element CONN_ISP
+ChangePinName(CONN_ISP, 6, 6)
+ChangePinName(CONN_ISP, 5, 5)
+ChangePinName(CONN_ISP, 4, 4)
+ChangePinName(CONN_ISP, 3, 3)
+ChangePinName(CONN_ISP, 2, 2)
+ChangePinName(CONN_ISP, 1, 1)
+
+# Start of element C3
+ChangePinName(C3, 2, 2)
+ChangePinName(C3, 1, 1)
+
+# Start of element CONN_SLOW_CONTROL
+ChangePinName(CONN_SLOW_CONTROL, 6, 6)
+ChangePinName(CONN_SLOW_CONTROL, 5, 5)
+ChangePinName(CONN_SLOW_CONTROL, 4, 4)
+ChangePinName(CONN_SLOW_CONTROL, 3, 3)
+ChangePinName(CONN_SLOW_CONTROL, 2, 2)
+ChangePinName(CONN_SLOW_CONTROL, 1, 1)
+
+# Start of element C11
+ChangePinName(C11, 2, 2)
+ChangePinName(C11, 1, 1)
+
+# Start of element C1
+ChangePinName(C1, 2, 2)
+ChangePinName(C1, 1, 1)
+
+# Start of element C7
+ChangePinName(C7, 2, 2)
+ChangePinName(C7, 1, 1)
+
+# Start of element U1
+ChangePinName(U1, 2, OUT)
+ChangePinName(U1, 3, VCC)
+ChangePinName(U1, 1, GND)
+
+# Start of element C6
+ChangePinName(C6, 2, 2)
+ChangePinName(C6, 1, 1)
+
+# Start of element R2
+ChangePinName(R2, 1, 1)
+ChangePinName(R2, 2, 2)
+
+# Start of element D0
+ChangePinName(D0, 2, 2)
+ChangePinName(D0, 1, 1)
+
+# Start of element R1
+ChangePinName(R1, 1, 1)
+ChangePinName(R1, 2, 2)
+
+# Start of element R8
+ChangePinName(R8, 1, 1)
+ChangePinName(R8, 2, 2)
+
+# Start of element U2
+ChangePinName(U2, 1, VCC)
+ChangePinName(U2, 14, GND)
+ChangePinName(U2, 6, "PA7(/SS)")
+ChangePinName(U2, 7, "PA6(MOSI)")
+ChangePinName(U2, 8, "PA5(MISO)")
+ChangePinName(U2, 9, "PA4(SCK)")
+ChangePinName(U2, 10, "PA3(XCK0)")
+ChangePinName(U2, 11, "PA2(RXD0)")
+ChangePinName(U2, 12, "PA1(TXD0)")
+ChangePinName(U2, 13, "PA0(AREF)")
+ChangePinName(U2, 4, "PB3(/RESET)")
+ChangePinName(U2, 5, "PB2(CLKO)")
+ChangePinName(U2, 3, "PB1(XTAL2)")
+ChangePinName(U2, 2, "PB0(XTAL1)")
+
+# Start of element U3
+ChangePinName(U3, 6, GND)
+ChangePinName(U3, 7, N.C.)
+ChangePinName(U3, 8, A)
+ChangePinName(U3, 9, B)
+ChangePinName(U3, 10, VCC)
+ChangePinName(U3, 5, DI)
+ChangePinName(U3, 4, /RE)
+ChangePinName(U3, 3, DE)
+ChangePinName(U3, 2, RO)
+ChangePinName(U3, 1, VL)
